98e1f37a2cf7c4a13b46b5b8d45db8a4bfc6e0f4
redawaseem <65788008+redawaseem@users.noreply.github.com>
2020-09-17 17:47:42 -0400

    updated js + added basic styles

--- script.js
+++ script.js
@@ -3,34 +3,44 @@ const movieApp = {};
 movieApp.key = "333bbafba7eed28cb6ecba2c4caeed82";
 
 
-movieApp.getMovies = function() {
+movieApp.getMovies = function(movie) {
     $.ajax({
         url: `https://api.themoviedb.org/3/discover/movie`,
         method: "GET",
         dataType: "JSON",
         data: {
             api_key: movieApp.key,
-            with_genres: 27,
+            with_genres: movie,
         }
-    }).then((results) => {
-        console.log(results);
+    }).then((res) => {
+        console.log(res);
+        movieApp.displayResults(res);
     })
-
 }
 
-movieApp.getMovies();
+movieApp.displayResults = (movies) => {
+    movies.results.forEach((movie) => {
+        $('.gallery').html(`<li><p>${movie.title}</p><div class="posterContainer"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="${movie.title} Movie Poster"></div></li>`);
+    })
+}
 
+movieApp.getGenre = () => {
+    $('#movieGenre').on('change', function() {
+        const movie = $(this).val();
+        console.log(movie);
+        movieApp.getMovies(movie);        
+    })
+} 
 
+//here is where we call the thigns we need to do once the page is ready
 
-
-
-
-// movieApp.init {
-//     //here is where we call the thigns we need to do once the page is ready
-// }
-
-
+movieApp.init = () => {
+    movieApp.getGenre();
+    movieApp.getMovies();
+}
 
 $(function() {
-    // movieApp.init();
+    movieApp.init();
 })
+
+// *NOTE: NEED HELP WITH FOR EACH LOOP (SEPARATING EACH ARRAY)
