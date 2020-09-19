961bbe67eca1c76cd042763f2da824377795f29e
Thomas <t.garrison333@gmail.com>
2020-09-18 20:15:38 -0400

    array append fix, page randomizer basic flex styles

--- script.js
+++ script.js
@@ -3,34 +3,47 @@ const movieApp = {};
 movieApp.key = "333bbafba7eed28cb6ecba2c4caeed82";
 
 
-movieApp.getMovies = function(movie) {
+movieApp.getMovies = function (movie = 28) {
     $.ajax({
         url: `https://api.themoviedb.org/3/discover/movie`,
         method: "GET",
         dataType: "JSON",
         data: {
+            page: movieApp.randomPage,
             api_key: movieApp.key,
             with_genres: movie,
         }
     }).then((res) => {
         console.log(res);
+        $('.gallery').empty();
         movieApp.displayResults(res);
     })
 }
 
+movieApp.randomPage = () => {
+    return Math.random() * (50 - 1) + 1;
+}
+
+
 movieApp.displayResults = (movies) => {
     movies.results.forEach((movie) => {
-        $('.gallery').html(`<li><p>${movie.title}</p><div class="posterContainer"><img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="${movie.title} Movie Poster"></div></li>`);
+        $('.gallery').append(`
+        <li>
+            <p>${movie.title}</p>
+            <div class="posterContainer">
+                <img src="https://image.tmdb.org/t/p/original${movie.poster_path}" alt="${movie.title} Movie Poster">
+            </div>
+        </li>`);
     })
 }
 
 movieApp.getGenre = () => {
-    $('#movieGenre').on('change', function() {
+    $('#movieGenre').on('change', function () {
         const movie = $(this).val();
         console.log(movie);
-        movieApp.getMovies(movie);        
+        movieApp.getMovies(movie);
     })
-} 
+}
 
 //here is where we call the thigns we need to do once the page is ready
 
@@ -39,8 +52,6 @@ movieApp.init = () => {
     movieApp.getMovies();
 }
 
-$(function() {
+$(function () {
     movieApp.init();
 })
-
-// *NOTE: NEED HELP WITH FOR EACH LOOP (SEPARATING EACH ARRAY)
